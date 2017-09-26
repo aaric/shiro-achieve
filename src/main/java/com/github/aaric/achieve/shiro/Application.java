8648a07f47bdcb2073381584cbf60f2ddbe84013
aaric <vipaaric@gmail.com>
2017-09-26 14:31:12 +0800

    init

---
+++ src/main/java/com/github/aaric/achieve/shiro/Application.java
@@ -0,0 +1,18 @@
+package com.github.aaric.achieve.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot Launcher.
+ *
+ * @author Aaric, created on 2017-09-26T12:40.
+ * @since 1.0-SNAPSHOT
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/com/github/aaric/achieve/shiro/ShiroTest.java
@@ -0,0 +1,10 @@
+package com.github.aaric.achieve.shiro;
+
+/**
+ * ShiroTest
+ *
+ * @author Aaric, created on 2017-09-26T12:44.
+ * @since 1.0-SNAPSHOT
+ */
+public class ShiroTest {
+}
