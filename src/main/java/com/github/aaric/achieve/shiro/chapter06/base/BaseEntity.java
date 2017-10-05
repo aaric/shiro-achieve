86c8ba6b0c0d4750220b5f59de850970ac2a57a0
Aaric <vipaaric@gmail.com>
2017-10-05 18:20:44 +0800

    chapter06-u

---
+++ src/main/java/com/github/aaric/achieve/shiro/chapter06/base/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.github.aaric.achieve.shiro.chapter06.base;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.io.Serializable;
+
+/**
+ * BaseEntity
+ *
+ * @author Aaric, created on 2017-10-05T14:02.
+ * @since 1.0-SNAPSHOT
+ */
+public abstract class BaseEntity implements Serializable {
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+}
